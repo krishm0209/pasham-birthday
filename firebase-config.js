cb7c108a52c40f6ee5fdc89ab51fc5c5360f02cb
krishm0209 <krish.makhija2@gmail.com>
2025-02-26 18:14:33 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZOHI0jm-V22DXpsFYbuve8-PJaQJpPIs",
+  authDomain: "pashambirthday.firebaseapp.com",
+  databaseURL: "https://pashambirthday-default-rtdb.firebaseio.com",
+  projectId: "pashambirthday",
+  storageBucket: "pashambirthday.firebasestorage.app",
+  messagingSenderId: "669962150729",
+  appId: "1:669962150729:web:2259655e27bcea7c80b098"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
